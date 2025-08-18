57b5ecee2d2b834ea50cb2f9d0c3691ea2588de5
Keeghan Campbell <keeghancampbell@gmail.com>
2025-08-18 22:13:15 +1200

    shona feedback

--- JurlinaContractorsWebsite/src/constants/compliance-and-certificates.consts.ts
+++ JurlinaContractorsWebsite/src/constants/compliance-and-certificates.consts.ts
@@ -1,7 +1,7 @@
 import { TCertificatesAndCompliance } from "../about-us/compliance-and-certificates.types";
 
 export const certificatesAndCompliances: TCertificatesAndCompliance = [
-    'Licensed Earthmoving Contractor (NZTA Approved)',
+    'Licensed Earthmoving Contractor',
     'Site Safe Certified',
     'Health & Safety Accredited',
     'Environmental Management Practices'
