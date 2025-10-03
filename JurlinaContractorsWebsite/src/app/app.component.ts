a0e22ce516627644689872e26df5e02dd40f0975
Keeghan Campbell <keeghancampbell@gmail.com>
2025-10-03 21:15:50 +1300

    Add title and meta tags

--- JurlinaContractorsWebsite/src/app/app.component.ts
+++ JurlinaContractorsWebsite/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { servicesOffered } from '../services/services.consts';
 import { ServiceComponent } from '../services/service/service.component';
@@ -9,6 +9,7 @@ import { ProjectsComponent } from "../projects/projects.component";
 import { HeroComponent } from "../hero/hero.component";
 import { NavComponent } from "../nav/nav.component";
 import { ServicesComponent } from "../services/services.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,23 @@ import { ServicesComponent } from "../services/services.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  services = servicesOffered;
+export class AppComponent implements OnInit {
+  readonly title = 'Jurlina Contractors | Earthmoving & Excavation Auckland';
+
+  constructor(private titleService: Title, private metaService: Meta) { }
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+
+    // Add meta tags
+    this.metaService.addTags([
+      { name: 'description', content: 'Professional contracting services in Auckland. Quality guaranteed.' },
+      { name: 'keywords', content: 'contractors, Auckland, construction, Jurlina' },
+      { name: 'author', content: 'Jurlina Contractors' },
+      { property: 'og:title', content: 'Jurlina Contractors' },
+      { property: 'og:description', content: 'Trusted contracting services in Auckland.' },
+      { property: 'og:type', content: 'website' }
+    ]);
+
+    this.metaService.addTag({ rel: 'canonical', href: 'https://jurlinacontractors.co.nz' }, true);
+  }
 }
